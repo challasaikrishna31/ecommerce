7933bb52047e5f61344c5ad382e66e733826d170
skchalla <skchalla@DESKTOP-NU9V6HJ>
2022-08-24 12:20:45 +0530

    Added Layout,NavBar,Footer

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
-const Layout = () => {
-  return <div>Layout</div>;
+import Head from "next/head";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Sai Digital Store</title>
+      </Head>
+      <header>
+        <NavBar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
